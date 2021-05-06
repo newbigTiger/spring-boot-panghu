244b607be3f38f1b7ef7591dc0311424573b9b12
胖虎 <760013429@qq.com>
2021-05-06 09:54:35 +0800

    ffffffff

--- src/main/java/com/example/demo/Utils/MyWebMvcConfigurer.java
+++ src/main/java/com/example/demo/Utils/MyWebMvcConfigurer.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author 胖虎
- * @date 2021/5/5 下午 1:15
+ * @date 2021/5/3 下午 1:15
  */
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
