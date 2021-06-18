b0ba36fdad4f7c4a8f752e5c69b31eb94b46e890
newbigTiger <760013429@qq.com>
2021-06-18 10:46:33 +0800

    rabbitMq

---
+++ src/main/java/com/example/demo/rabbitmq/Producer.java
@@ -0,0 +1,33 @@
+package com.example.demo.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    public static void main(String[] args) throws IOException, TimeoutException
+    {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        //设置 RabbitMQ 地址
+        factory.setHost("47.98.207.131");
+        //建立到代理服务器到连接
+        Connection conn = factory.newConnection();
+        //获得信道
+        Channel channel = conn.createChannel();
+        //声明交换器
+        String exchangeName = "hello-exchange";
+        channel.exchangeDeclare(exchangeName, "direct", true);
+        String routingKey = "hola";
+        //发布消息
+        byte[] messageBodyBytes = "quit".getBytes();
+        channel.basicPublish(exchangeName, routingKey, null, messageBodyBytes);
+        channel.close();
+        conn.close();
+    }
+
+}
